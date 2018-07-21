5a4b92707cdf38db7980562bd0cdabf527403c9e
ysj0411 <37933313+ysj0411@users.noreply.github.com>
2018-07-21 20:00:36 +0800

    增加了最常见的图处理代码，如度数，最大度数，平均度数，自环数，字符串表达

--- Graph.java
+++ Graph.java
@@ -28,12 +28,48 @@ public class Graph {//图表；即图
     	adj[w].add(v);//
     	E++;//边加一，因为每次都用到两次边所以只要加一
     }
+    public static int degree(Graph G,int v){//度数，即依附于它的边的总数
+    	int degree =0;
+    	for(int w:G.adj(v)) degree++;//遍历与它相邻的顶点，即为度数
+    	return degree;
+    	
+    }
+    public static int maxDegree(Graph G){//最大度数
+    	int max=0;
+    	for(int v=0;v<G.V();v++)
+    		if(degree(G,v)>max)//各顶点度数比较
+    			max=degree(G,v);
+    	return max;
+    }
+    public static double avgDegree(Graph G){
+    	return 2.0*G.E()/G.V();//因为两个顶点公用一条边，所以要先乘2
+    }
+    public static int numberOFSelfLoops(Graph G){//自环数
+    	int count = 0;//计数器
+    	for(int v=0;v<G.V();v++)
+    		for(int w: G.adj(v))
+    			if(v==w) count++;//这和添加顶点链表有关，v加到w中，w又加到v中所以自环时就会把边记录两次即v==w，w==v
+    	return count/2;
+    }
+    public String toString(){//用字符串表示图
+    	String s=V + " vertics" + E + "edges\n";//几个顶点，几条边
+    	for ( int v= 0;v<V;v++)//外循环次数为顶点数
+    	{
+    		s+=v+" :";//不同顶点显示，举例就是0：下方就是把他相邻的顶点都显示出来
+    		for(int w : this.adj(v))//显示与当前顶点相邻的顶点
+    			s+=w+"  ";
+    		s+="\n";//结果就如0:1 2 3 4 5(：后面为相邻的顶点）。循环为行hang
+    	}
+    	return s;
+    }
     public Iterable<Integer> adj(int v){
     	return adj[v];
     }
+    
     public static void main(String[] args) {
         In in = new In(args[0]);//
         Graph G = new Graph(in);//
         StdOut.println(G);
     }  	
 }
+
