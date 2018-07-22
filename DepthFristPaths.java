847c078bf7f583d0aea6fc1d010292a69fdfc852
ysj0411 <37933313+ysj0411@users.noreply.github.com>
2018-07-22 21:05:51 +0800

    使用深度优先搜索查找图中的路径
    顺便附上要引用的栈类

---
+++ DepthFristPaths.java
@@ -0,0 +1,34 @@
+﻿
+public class DepthFirstPaths {//dfs,深度优先搜索查找图中的路径
+     private boolean[] marked;//标记这个顶点是否被调用过
+     private int[] edgeTo;//当前结点的父节点，即上一个顶点
+     private final int s;//起点，只能被赋值一次
+     public DepthFirstPaths(Graph G, int s){
+    	 marked=new boolean[G.V()];
+    	 edgeTo= new int[G.V()];
+    	 this.s=s;//起点赋值
+    	 dfs(G,s);// 递归
+    	 
+     }
+     private void dfs(Graph G,int v){
+    	 marked[v]=true;//标记当前顶点
+    	 for(int w:G.adj(v))//遍历与当前相邻的顶点
+    		 if(!marked[v]) {// 如果为标记
+    			 edgeTo[w]=v;//记录父结点
+    			 dfs(G,s);// 递归
+    		 }
+     }
+     public boolean hasPathTo(int v){
+    	 return marked[v];
+     }
+     public Iterable<Integer> pathTo(int v){
+    	 if(!hasPathTo(v)) return null;
+    	 Stack<Integer> path = new Stack<Integer>();//构造栈
+    	 for(int x=v;x!=s;x = edgeTo[x])//获取当前的要输出的路径的终点
+    		 path.push(x);//把当前结点压入栈，接着压入父结点，一直压到起点
+    	 path.push(s);// 压入起点
+    	 return path;//返回栈
+    		 
+     }
+
+}
---
+++ Stack.java
@@ -0,0 +1,46 @@
+﻿import java.util.Iterator;
+
+
+
+public class Stack<Item > implements Iterable<Item> {
+     private Node first;
+     private int N;
+     private class Node{
+    	 Item item;
+    	 Node next;
+     }
+     public boolean isEmpty(){return first==null;}
+     public int size(){return N;}
+     public void push(Item item){
+    	 Node oldfirst=first;
+    	 first=new Node();
+    	 first.item=item;
+    	 first.next=oldfirst;
+    	 N++;
+     }
+     public Item pop(){
+    	 Item item=first.item;
+    	 first=first.next;
+    	 N--;
+    	 return item;
+    	 
+     }
+     public Iterator<Item> iterator() {
+			
+			return new ListIterator();//保证了类必然会实现hashNext(),next()和remove()供用例的foreach语法使用、
+		}
+		private class ListIterator implements Iterator<Item>{//维护了一个实例变量currrent来记录链表当前的结点。hashNext()方法检测current是否为null，next()方法会保存当前元素的引用。将current变量指向链表的下个结点并返回所保存的引用
+			private Node current =first;
+			public boolean hasNext(){
+				return current !=null;
+			}
+			public void remove(){}
+			public Item next(){
+				Item item=current.item;
+				current = current.next;
+				return item;
+			}
+		
+		
+		}
+}
